819fa42e95bfbda64ab4b0f74277fd761abc2dec
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-06-15 22:11:08 -0300

    resuelvo ejercicios de compararar numeros enteros

--- app.js
+++ app.js
@@ -18,3 +18,25 @@ else if (compra > 300 && compra <= 500) {
     console.log (" tienes un 15% de descuento");
     console.log ("el descuento es de " + descuento +" dolares.");
 }
+
+
+
+
+
+//comparar numeros enteros
+let num1 = parseInt(prompt("Ingrese el primer numero entero:"));
+let num2 = parseInt(prompt("Ingrese el segundo numero entero:"));
+
+if (num1 > num2) {
+    console.log ("El numero mayor es: " + num1);
+}
+     else if (num2 > num1) {  
+    console.log ("El numero mayor es: " + num2);
+  }
+    else {  
+    console.log ("Ambos numeros son iguales");
+} 
+
+
+// notas de alumnos
+
