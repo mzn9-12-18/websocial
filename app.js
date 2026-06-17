7d64d7578f20677c584b0767e85ed8c1eedc629a
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-06-17 17:59:51 -0300

    agrego calculo del total a pagar tras aplicar descuentos

--- app.js
+++ app.js
@@ -5,18 +5,24 @@ if (compra < 100) {
 }
 else if (compra >= 100 && compra <= 300) {
     let descuento = compra * 0.05;
-    console.log ("Si tienes un 5% de descuento");
+    let total = compra - descuento;
+    console.log ("tienes un 5% de descuento");
     console.log ("el descuento es de " + descuento +" dolares.");
+    console.log("Total a pagar: " + total + " dólares.");
 }
 else if (compra > 300 && compra <= 500) {
     let descuento = compra * 0.10;
+    let total = compra - descuento;
     console.log (" tienes un 10% de descuento");
     console.log ("el descuento es de " + descuento +" dolares.");
+    console.log("Total a pagar: " + total + " dólares.");
  }
  else if (compra > 500) { 
     let descuento = compra * 0.15;
+    let total = compra - descuento;
     console.log (" tienes un 15% de descuento");
     console.log ("el descuento es de " + descuento +" dolares.");
+    console.log("Total a pagar: " + total + " dólares.");
 }
 
 
