a35b8d37f64c4eea37ead046e2598232636a6900
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-06-27 19:45:42 -0300

    agrego cuenta regresiva desde el 10 hasta el 1.

--- 02-while.js
+++ 02-while.js
@@ -15,4 +15,22 @@ while (contador <= 10) {
   contador++;
 }
 
+console.log( "----")
+/* cuenta regresiva desde el 10 al 1
 
+--ALGORITMO--
+- iniciar programa
+1- definir el numero inicial de la cuenta regresiva
+2- definir el numero final.
+3- mientras no se llegue al numero final:
+  - mostrar en consola el numero.
+  - restar 1 al contador
+-- finalizar el programa. */
+
+
+let contadorRegresivo = 10;
+
+while (contadorRegresivo >= 1) {
+  console.log(contadorRegresivo);
+  contadorRegresivo--;
+}
