28e8a2a21c4afd34274a7875d08a1fcc4107de99
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-06-27 19:26:58 -0300

    agrego contador desde el 1 al 10 utlizando el ciclo while.

---
+++ 02-while.js
@@ -0,0 +1,18 @@
+/* --ALGORITMO--
+1- iniciar programa.
+2- definir numero inicial para el contador.
+3- definir el numero final.
+4- mientras el contador no llegue al numero final:
+   - mostrar numero en consola.
+   - incrementar el contador
+5- finalizar programa. */
+
+
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
+
+
