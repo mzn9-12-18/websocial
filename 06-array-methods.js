a830888fceb9e564cd8cf7dae1e3c511f1c83d76
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-07-10 21:08:59 -0300

    agrego ejercicio 2 de el filtro de seguridad con el metodo filter

--- 06-array-methods.js
+++ 06-array-methods.js
@@ -18,3 +18,28 @@ const temperaturas = [0, 10, 20, 30, 40];
 const convertirAFahrenheit = (temperaturas) => 
      temperaturas.map((temperatura) =>  (temperatura * 9 / 5) + 32);
 console.log(convertirAFahrenheit(temperaturas));
+
+
+console.log("")
+
+/*
+Ejercicio 2: El Filtro de Seguridad ( filter)
+
+--ALGORITMO--
+
+-- iniciar programa 
+
+1- Crear una función llamada obtenerMayoresDeEdad.
+2- Recibir un array de edades.
+3- recorrer el array utilizando (filter).
+4- devolver un nuevo array con las edades mayores o iguales a 18.
+5- mostrar resultado en consola. 
+  finalizar programa. -- */
+
+const edades = [12, 18, 25, 10, 30, 15];
+
+const obtenerMayoresDeEdad = (edades) =>
+    edades.filter((edad) => edad >= 18);
+
+console.log(obtenerMayoresDeEdad(edades));
+
