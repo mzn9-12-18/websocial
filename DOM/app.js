bbcf0dcc3acddb5038925e293f1356f6d5713d9e
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-07-16 21:24:35 -0300

    agrego ejercicio de captura de eventos

---
+++ DOM/app.js
@@ -0,0 +1,7 @@
+const btnAgregar = document.querySelector("#btnAgregar");
+const inputTarea = document.querySelector("#inputTarea");
+
+btnAgregar.addEventListener("click", () => {
+    console.log(inputTarea.value);
+});
+
