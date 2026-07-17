dfc8318abad3b6086bfc212d019816c948b693cd
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-07-17 17:17:40 -0300

    agrego funcionalidad para borrar tareas con el boton X

--- DOM/app.js
+++ DOM/app.js
@@ -2,19 +2,21 @@ const btnAgregar = document.querySelector("#btnAgregar");
 const inputTarea = document.querySelector("#inputTarea");
 const listaTareas = document.querySelector("#listaTareas");
 
-
 btnAgregar.addEventListener("click", () => {
     console.log(inputTarea.value);
 
     const li = document.createElement("li");
-
+    
     li.className = "list-group-item d-flex justify-content-between";
 
     li.innerHTML = `
         <span>${inputTarea.value}</span>
         <button class=“btn btn-danger btn-sm”>X</button>
     `;
-
+    const botonBorrar = li.querySelector("button");
+   botonBorrar.addEventListener("click" , () => {
+    li.remove();
+   });
     listaTareas.append(li);
     inputTarea.value = "";
     inputTarea.focus();
