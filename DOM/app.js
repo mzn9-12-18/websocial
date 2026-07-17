7a83990382f00e22e97344f8e9865309a86d3dd2
Brandon Yuthiel <monzonbrandon555@gmail.com>
2026-07-16 22:36:37 -0300

    feat : agregar creacion dinamica de tareas con DOM

--- DOM/app.js
+++ DOM/app.js
@@ -1,7 +1,18 @@
 const btnAgregar = document.querySelector("#btnAgregar");
 const inputTarea = document.querySelector("#inputTarea");
+const listaTareas = document.querySelector("#listaTareas");
 
 btnAgregar.addEventListener("click", () => {
     console.log(inputTarea.value);
-});
 
+    const li = document.createElement("li");
+
+    li.className = "list-group-item d-flex justify-content-between";
+
+    li.innerHTML = `
+        <span>${inputTarea.value}</span>
+        <button class=“btn btn-danger btn-sm”>X</button>
+    `;
+
+    listaTareas.append(li);
+});
